1a75ca96a1fb7401cafb8494a01f326791fd4836
navraj.singh <navraj.singh@infodartmail.com>
2018-06-19 20:42:37 +0530

    Code Merge

--- Founders-2.0/Founders/Program.cs
+++ Founders-2.0/Founders/Program.cs
@@ -825,13 +825,6 @@ CommandOption echo = commandLineApplication.Option(
             Console.Out.WriteLine(" Export:      " + FS.ExportFolder);
             Console.Out.WriteLine(" Lost:        " + FS.LostFolder);
 
-            //QRCodeGenerator qrGenerator = new QRCodeGenerator();
-            //QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
-            //QRCode qrCode = new QRCode(qrCodeData);
-            //Bitmap qrCodeImage = qrCode.GetGraphic(20);
-
-            //qrCodeImage.Save("qrcode.jpeg");
-
 
         } // end show folders
 
